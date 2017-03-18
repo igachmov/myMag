633be1e2d4130d6943cde19c70c4432fd990424b
simo-andreev <simeon.zlatanov.andreev@abv.bg>
2017-03-19 01:32:41 +0200

    Added temporary buttons in the Home activity, leading to the other activities, for easier navigation in-dev. Started work on ProductActivity and Toolbar.

--- app/src/main/java/com/mymag/mymag/model/activities/HomeActivity.java
+++ app/src/main/java/com/mymag/mymag/model/activities/HomeActivity.java
@@ -1,7 +1,13 @@
 package com.mymag.mymag.model.activities;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 import com.mymag.mymag.R;
 
@@ -11,5 +17,77 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar tb = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(tb);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setHomeAsUpIndicator(R.mipmap.ic_launcher_round);
+        ab.setDisplayShowTitleEnabled(false);
+
+
+        Button cart = (Button) findViewById(R.id.cart_redirect);
+        Button category = (Button) findViewById(R.id.category_redirect);
+        Button checkOut = (Button) findViewById(R.id.check_out_redirect);
+        Button logIn = (Button) findViewById(R.id.log_in_redirect);
+        Button register = (Button) findViewById(R.id.register_redirect);
+        Button product = (Button) findViewById(R.id.product_redirect);
+        Button productList = (Button) findViewById(R.id.product_list_redirect);
+        Button user = (Button) findViewById(R.id.user_redirect);
+
+
+        Button.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int id = v.getId();
+                Class destination = HomeActivity.class;
+
+                switch (id){
+                    case R.id.cart_redirect:
+                        destination = CartActivity.class;
+                        break;
+                    case R.id.category_redirect:
+                        destination = CartActivity.class;
+                        break;
+                    case R.id.check_out_redirect:
+                        destination = CheckOutActivity.class;
+                        break;
+                    case R.id.log_in_redirect:
+                        destination = LoginActivity.class;
+                        break;
+                    case R.id.register_redirect:
+                        destination = RegisterActivity.class;
+                        break;
+                    case R.id.product_redirect:
+                        destination = ProductActivity.class;
+                        break;
+                    case R.id.product_list_redirect:
+                        destination = ProductListActivity.class;
+                        break;
+                    case R.id.user_redirect:
+                        destination = UserActivity.class;
+                        break;
+                }
+                startActivity(new Intent(HomeActivity.this,destination));
+            }
+        };
+
+        cart.setOnClickListener(clickListener);
+        category.setOnClickListener(clickListener);
+        checkOut.setOnClickListener(clickListener);
+        logIn.setOnClickListener(clickListener);
+        product.setOnClickListener(clickListener);
+        productList.setOnClickListener(clickListener);
+        register.setOnClickListener(clickListener);
+        user.setOnClickListener(clickListener);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_app_bar,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/mymag/mymag/model/activities/ProductActivity.java
+++ app/src/main/java/com/mymag/mymag/model/activities/ProductActivity.java
@@ -1,17 +1,48 @@
 package com.mymag.mymag.model.activities;
 
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.mymag.mymag.R;
 
 public class ProductActivity extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        Toolbar tb = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(tb);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        ab.setDisplayUseLogoEnabled(true);
+
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_app_bar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public static class ProductDescriptionFragment extends Fragment{
+
+
+
+    }
+
+    public static class ProductSpecificationsFragment extends Fragment{
+
+    }
+
 }
+
