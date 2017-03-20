6af87826e068bbdbb9de5368f3dcdae96d543f0c
simo-andreev <simeon.zlatanov.andreev@abv.bg>
2017-03-20 04:02:56 +0200

    Some touch-up on product visualisation

--- app/src/main/java/com/mymag/mymag/model/activities/ProductActivity.java
+++ app/src/main/java/com/mymag/mymag/model/activities/ProductActivity.java
@@ -1,8 +1,10 @@
 package com.mymag.mymag.model.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,53 +12,59 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListViewCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mymag.mymag.R;
-import com.mymag.mymag.model.products.Computer;
 import com.mymag.mymag.model.products.Product;
+import com.mymag.mymag.model.users.User;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProductActivity extends AppCompatActivity {
 
+    private static String PRODUCT_KEY;
+    //Activity's toolbar
     private Toolbar tb;
+    //Tab container for swipable pages
     private TabLayout tabLayout;
+    //ViewPager managing swipable pages
     private ViewPager viewPager;
-
-    private Product product;
-
     private ProductPagerAdapter pagerAdapter;
+    //Product which to visualize
+    private Product product;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
 
+        PRODUCT_KEY = getString(R.string.PRODUCT_KEY);
+
+        //Get product which to visualize
         Intent i = getIntent();
+        product = (Product) i.getSerializableExtra(PRODUCT_KEY);
+        if (product == null) finish();
 
-        product = (Product) i.getSerializableExtra("Product");
+        pagerAdapter = new ProductPagerAdapter(getSupportFragmentManager(), product);
 
-        pagerAdapter = new ProductPagerAdapter(getSupportFragmentManager(),product);
-
+        //Set-up Toolbar & ViewPager
         tb = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(tb);
-
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
         ab.setDisplayUseLogoEnabled(true);
-
 
         viewPager = (ViewPager) findViewById(R.id.product_view_pager);
         viewPager.setAdapter(pagerAdapter);
@@ -64,6 +72,16 @@ public class ProductActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.product_tab_layout);
         tabLayout.setupWithViewPager(viewPager);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.buy_button);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User.user.addToCart(product);
+                Toast.makeText(ProductActivity.this, product.getName() + " added to cart.", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ProductActivity.this, CartActivity.class));
+            }
+        });
     }
 
 
@@ -73,13 +91,16 @@ public class ProductActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    /**
+     * Displays the product photo and product text description with buy button and price at the bottom.
+     */
     public static class ProductDescriptionFragment extends Fragment {
         private Product product;
 
         public static Fragment newInstance(Product p) {
             Fragment f = new ProductDescriptionFragment();
             Bundle b = new Bundle();
-            b.putSerializable("@strings:PRODUCT_KEY",p);
+            b.putSerializable(PRODUCT_KEY, p);
             f.setArguments(b);
             return f;
         }
@@ -89,77 +110,101 @@ public class ProductActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
             View v = inflater.inflate(R.layout.fragment_product_description, container, false);
+
             TextView description = (TextView) v.findViewById(R.id.product_description);
             ImageView imageView = (ImageView) v.findViewById(R.id.product_photo);
             TextView price = (TextView) v.findViewById(R.id.product_price);
 
-            product = (Product) getArguments().getSerializable("@strings:PRODUCT_KEY");
+            product = (Product) getArguments().getSerializable(PRODUCT_KEY);
 
-            price.setText(String.valueOf(product.getPrice()));
             description.setText(product.getDescription());
+            price.setText(String.valueOf(product.getPrice()));
 
             return v;
         }
     }
 
+    /**
+     * Displays a table of the product's detailed characteristics
+     */
     public static class ProductSpecificationFragment extends Fragment {
-        public static Fragment newInstance() {
-            return new ProductSpecificationFragment();
-        }
-
         private Product product;
+
+        private ListViewCompat listView;
 
         public static Fragment newInstance(Product p) {
             Fragment f = new ProductSpecificationFragment();
+
             Bundle b = new Bundle();
-            b.putSerializable("@strings:PRODUCT_KEY",p);
+            b.putSerializable(PRODUCT_KEY, p);
             f.setArguments(b);
+
             return f;
         }
 
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View v = inflater.inflate(R.layout.fragment_product_specification,container,false);
+            View v = inflater.inflate(R.layout.fragment_product_specification, container, false);
 
-            TableLayout tl = (TableLayout) v.findViewById(R.id.product_specs_table);
-            product = (Product) getArguments().getSerializable("@strings:PRODUCT_KEY");
+            product = (Product) getArguments().getSerializable(PRODUCT_KEY);
+            listView = (ListViewCompat) v.findViewById(R.id.product_specs_list);
 
-            HashMap<String, String> specs = product.getSpecs();
-
-            for (Map.Entry<String,String> entry : specs.entrySet()) {
-//                TableLayout.LayoutParams tableParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT);
-//                TableRow.LayoutParams rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
-//
-//                TableLayout tableLayout = new TableLayout(context);
-//                tableLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));// assuming the parent view is a LinearLayout
-//
-//                TableRow tableRow = new TableRow(context);
-//                tableRow.setLayoutParams(tableParams);// TableLayout is the parent view
-//
-//                TextView textView = new TextView(context);
-//                textView.setLayoutParams(rowParams);// TableRow is the parent view
-//
-//                tableRow.addView(textView);
-
-                TableRow tr = new TableRow(getActivity());
-
-                TextView specName = new TextView(getActivity());
-                specName.setText(entry.getKey());
-                tr.addView(specName);
-
-                TextView specValue = new TextView(getActivity());
-                specValue.setText(entry.getValue());
-                tr.addView(specValue);
-
-                tl.addView(tr);
-            }
+            ProductSpecsListAdapter listAdapter = new ProductSpecsListAdapter(getContext(), product);
+            listView.setAdapter(listAdapter);
 
             return v;
         }
     }
 
 }
+
+
+class ProductSpecsListAdapter extends BaseAdapter {
+
+    //Used ArrLst insted as HshMap (as Product.getSpecs() returns, to allow returning by position(index);
+    private ArrayList<Map.Entry<String, String>> specifications;
+
+    private Context c;
+
+    ProductSpecsListAdapter(Context context, Product p) {
+        specifications = new ArrayList<>();
+        specifications.addAll(p.getSpecs().entrySet());
+        c = context;
+    }
+
+    @Override
+    public int getCount() {
+        Log.e("specs.size() == ", String.valueOf(specifications.size()));
+
+        return this.specifications.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return specifications.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.view_product_specification, parent, false);
+
+        Map.Entry<String, String> spec = specifications.get(position);
+
+        ((TextView) row.findViewById(R.id.spec_title)).setText(spec.getKey());
+        ((TextView) row.findViewById(R.id.spec_cotent)).setText(spec.getValue());
+
+        return row;
+    }
+}
+
 
 class ProductPagerAdapter extends FragmentPagerAdapter {
 
